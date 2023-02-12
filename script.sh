b2566b25acf6893d55a79a043133988e29a6b064
MonPham <demondhunter93@gmail.com>
2023-02-12 16:43:35 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,15 +2,6 @@
 # shellcheck disable=SC1090
 
 source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
-
-printLogo
-
-read -r -p "Enter node moniker: " NODE_MONIKER
-read -s -p "Enter your keyring password: " KEYRING_PASSWORD
-printf "\n"
-read -s -p "Enter your tofnd password: " TOFND_PASSWORD
-printf "\n"
-
 CHAIN_ID="axelar-testnet-lisbon-3"
 CHAIN_HOME=".axelar_testnet"
 CHAIN_DENOM="uaxl"
@@ -19,6 +10,21 @@ AXELARD_BINARY_VERSION="v0.31.2"
 AXELARD_BINARY_PATH="$HOME/$CHAIN_HOME/bin/$AXELARD_BINARY_NAME"
 TOFND_VERSION="v0.10.1"
 CHEAT_SHEET="https://nodejumper.io/axelar-testnet/cheat-sheet"
+# if [ $INIT ] then
+#     printLogo
+
+#     read -r -p "Enter node moniker: " MONIKER
+#     read -s -p "Enter your keyring password: " PASSWORD
+#     printf "\n"
+#     read -s -p "Enter your tofnd password: " PASSWORD
+#     printf "\n"
+
+# fi
+
+
+
+
+
 
 printLine
 echo -e "Node moniker:    ${CYAN}$NODE_MONIKER${NC}"
@@ -57,26 +63,28 @@ echo "export AXELARD_HOME=$HOME/$CHAIN_HOME" >> "$HOME/.bash_profile"
 echo "export AXELARD_CHAIN_ID=$AXELARD_CHAIN_ID" >> "$HOME/.bash_profile"
 source "$HOME/.bash_profile"
 
+# if [ $INIT ] then
 # init chain
-axelard init "$NODE_MONIKER" --chain-id $CHAIN_ID --home "$HOME/$CHAIN_HOME"
+    axelard init "$NODE_MONIKER" --chain-id $CHAIN_ID --home "$HOME/$CHAIN_HOME"
 
 # override configs
-curl https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/configuration/app.toml > "$HOME/$CHAIN_HOME/config/app.toml"
-curl https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/configuration/config.toml > "$HOME/$CHAIN_HOME/config/config.toml"
-curl https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/resources/testnet/seeds.toml > "$HOME/$CHAIN_HOME/config/seeds.toml"
-curl https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/resources/testnet/genesis.json > "$HOME/$CHAIN_HOME/config/genesis.json"
-curl https://snapshots-testnet.nodejumper.io/axelar-testnet/addrbook.json > "$HOME/$CHAIN_HOME/config/addrbook.json"
-sed -i 's|^moniker *=.*|moniker = "'"$NODE_MONIKER"'"|g' "$HOME/$CHAIN_HOME/config/config.toml"
-sed -i 's|^external_address *=.*|external_address = "'"$(curl -s eth0.me)"':26656"|g' "$HOME/$CHAIN_HOME/config/config.toml"
+    curl https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/configuration/app.toml > "$HOME/$CHAIN_HOME/config/app.toml"
+    curl https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/configuration/config.toml > "$HOME/$CHAIN_HOME/config/config.toml"
+    curl https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/resources/testnet/seeds.toml > "$HOME/$CHAIN_HOME/config/seeds.toml"
+    curl https://raw.githubusercontent.com/axelarnetwork/axelarate-community/main/resources/testnet/genesis.json > "$HOME/$CHAIN_HOME/config/genesis.json"
+    curl https://snapshots-testnet.nodejumper.io/axelar-testnet/addrbook.json > "$HOME/$CHAIN_HOME/config/addrbook.json"
+    sed -i 's|^moniker *=.*|moniker = "'"$NODE_MONIKER"'"|g' "$HOME/$CHAIN_HOME/config/config.toml"
+    sed -i 's|^external_address *=.*|external_address = "'"$(curl -s eth0.me)"':26656"|g' "$HOME/$CHAIN_HOME/config/config.toml"
 
-PRUNING_INTERVAL=$(shuf -n1 -e 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97)
-sed -i 's|^pruning *=.*|pruning = "custom"|g' "$HOME/$CHAIN_HOME/config/app.toml"
-sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' "$HOME/$CHAIN_HOME/config/app.toml"
-sed -i 's|^pruning-interval *=.*|pruning-interval = "'$PRUNING_INTERVAL'"|g' "$HOME/$CHAIN_HOME/config/app.toml"
-sed -i 's|^snapshot-interval *=.*|snapshot-interval = 2000|g' "$HOME/$CHAIN_HOME/config/app.toml"
+    PRUNING_INTERVAL=$(shuf -n1 -e 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97)
+    sed -i 's|^pruning *=.*|pruning = "custom"|g' "$HOME/$CHAIN_HOME/config/app.toml"
+    sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' "$HOME/$CHAIN_HOME/config/app.toml"
+    sed -i 's|^pruning-interval *=.*|pruning-interval = "'$PRUNING_INTERVAL'"|g' "$HOME/$CHAIN_HOME/config/app.toml"
+    sed -i 's|^snapshot-interval *=.*|snapshot-interval = 2000|g' "$HOME/$CHAIN_HOME/config/app.toml"
 
-printCyan "5. Starting services and synchronization..." && sleep 1
-
+    printCyan "5. Starting services and synchronization..." && sleep 1
+    axelard tendermint unsafe-reset-all --home "$HOME/$CHAIN_HOME"
+# fi
 # sudo tee /etc/systemd/system/axelard.service > /dev/null << EOF
 # [Unit]
 # Description=Axelard Cosmos daemon
@@ -94,8 +102,8 @@ printCyan "5. Starting services and synchronization..." && sleep 1
 # EOF
 
 # ! set your TOFND_PASSWORD
-# screen -Smd tofn /usr/bin/sh -c 'echo "$TOFND_PASSWORD" | "$HOME/$CHAIN_HOME/bin/tofnd" -m existing -d $HOME/$CHAIN_HOME/.tofnd'
-# screen -Smd val /usr/bin/sh -c 'echo "$KEYRING_PASSWORD" | $AXELARD_BINARY_PATH vald-start --validator-addr \$(echo "$KEYRING_PASSWORD" | $AXELARD_BINARY_PATH keys show validator --home "$HOME/$CHAIN_HOME" --bech val -a) --log_level debug --chain-id $CHAIN_ID --from broadcaster --home "$HOME/$CHAIN_HOME"'
+screen -Smd tofn /usr/bin/sh -c 'echo "$TOFND_PASSWORD" | "$HOME/$CHAIN_HOME/bin/tofnd" -m existing -d $HOME/$CHAIN_HOME/.tofnd'
+screen -Smd val /usr/bin/sh -c 'echo "$KEYRING_PASSWORD" | $AXELARD_BINARY_PATH vald-start --validator-addr \$(echo "$KEYRING_PASSWORD" | $AXELARD_BINARY_PATH keys show validator --home "$HOME/$CHAIN_HOME" --bech val -a) --log_level debug --chain-id $CHAIN_ID --from broadcaster --home "$HOME/$CHAIN_HOME"'
 # sudo tee /etc/systemd/system/tofnd.service > /dev/null << EOF
 # [Unit]
 # Description=Tofnd daemon
@@ -129,7 +137,7 @@ printCyan "5. Starting services and synchronization..." && sleep 1
 # EOF
 
 # download fresh snapshot
-axelard tendermint unsafe-reset-all --home "$HOME/$CHAIN_HOME"
+
 
 # SNAP_NAME=$(curl -s https://snapshots-testnet.nodejumper.io/axelar-testnet/info.json | jq -r .fileName)
 # curl "https://snapshots-testnet.nodejumper.io/axelar-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.axelar_testnet"
@@ -143,8 +151,8 @@ axelard tendermint unsafe-reset-all --home "$HOME/$CHAIN_HOME"
 # sudo systemctl start vald
 
 printLine
-echo -e "Check $AXELARD_BINARY_NAME logs:    ${CYAN}sudo journalctl -u $AXELARD_BINARY_NAME -f --no-hostname -o cat ${NC}"
+# echo -e "Check $AXELARD_BINARY_NAME logs:    ${CYAN}sudo journalctl -u $AXELARD_BINARY_NAME -f --no-hostname -o cat ${NC}"
 echo -e "Check synchronization: ${CYAN}$AXELARD_BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
 echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
 
-sleep infinity
+
